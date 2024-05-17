5cca9c7fe457666a00cfad148a0ce35a9a664c68
AdII-dev <78079623+AdII-dev@users.noreply.github.com>
2024-05-17 08:45:57 +0200

    Add project files.

---
+++ Viragok/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.Security.Policy;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Viragok
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Virag> viragok = new();
+        public List<string> adat { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            using StreamReader sr = new(
+                path: @"..\..\..\src\flowers.txt",
+                encoding: System.Text.Encoding.UTF8);
+            while (!sr.EndOfStream) viragok.Add(new(sr.ReadLine()));
+            Feltolt(viragok);
+        }
+        private void Feltolt(List<Virag> viragok)
+        {
+            Viragok.Items.Clear();
+            foreach (var v in viragok)
+            {
+                ListBoxItem varos = new();
+                varos.Content = v._fajta;
+                Viragok.Items.Add(varos);
+            }
+            Ar.Items.Clear();
+            foreach (var v in viragok)
+            {
+                ListBoxItem varos = new();
+                varos.Content = v._ar;
+                Ar.Items.Add(varos);
+            }
+            Szin.Items.Clear();
+            foreach (var v in viragok)
+            {
+                ListBoxItem varos = new();
+                varos.Content = v._szin;
+                Szin.Items.Add(varos);
+            }
+        }
+
+        
+    }
+}
---
+++ Viragok/Virag.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viragok
+{
+    internal class Virag
+    {
+        public string _fajta;
+        public double _ar;
+        public string _szin;
+        public string _fajl;
+
+        public Virag(string fajta, double ar, string szin, string fajl)
+        {
+
+            _fajta = fajta;
+            _ar = ar;
+            _szin = szin;
+            _fajl = fajl;
+        }
+        public Virag(string sor)
+        {
+            var adatok = sor.Split(';').ToList();
+            _fajta = adatok[0];
+            _ar = double.Parse(adatok[1]);
+            _szin = adatok[2];
+            _fajl = adatok[3];
+        }
+    }
+}
